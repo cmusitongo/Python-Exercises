e2d79254fa572a7941370a7fad81c9b8afcf9a78
cmusitongo <chrismusitongo@gmail.com>
2017-09-05 10:43:33 +0100

    Added more exercises

---
+++ ex11s.py
@@ -0,0 +1,9 @@
+print "What is your favourite color?",
+color = raw_input()
+print "What is your favourite programming language?",
+language = raw_input()
+print "How many siblings do you have?",
+siblings = raw_input()
+
+print "So your favourite color is %r, your favourite language is %r and you have %r siblings." % (
+    color, language, siblings)
